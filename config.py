779647d7edccb0b50cb8370179c430b3b45150db
Chrys-cat <chrysolitecatalino+github@gmail.com>
2026-02-24 07:43:26 +0800

    can now run outside compiler

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 # ---------------- FILE CONFIG ---------------- #
 
-STUDENT_FILE = "students.csv"
-PROGRAM_FILE = "programs.csv"
-COLLEGE_FILE = "colleges.csv"
+import os
+
+_BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+STUDENT_FILE = os.path.join(_BASE_DIR, "students.csv")
+PROGRAM_FILE = os.path.join(_BASE_DIR, "programs.csv")
+COLLEGE_FILE = os.path.join(_BASE_DIR, "colleges.csv")
 
 STUDENT_FIELDS = ["id", "firstname", "lastname", "program_code", "year", "gender"]
 PROGRAM_FIELDS = ["code", "name", "college_code"]
